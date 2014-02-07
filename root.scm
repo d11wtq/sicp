87375b358e9ab30fb844a1d9f8006bb4baa0bf15
d11wtq <chris@w3style.co.uk>
2014-02-07 23:33:08 +1100

    Clean up with some lexical scoping

--- root.scm
+++ root.scm
@@ -2,17 +2,18 @@
 
 (define (root x n)
   "Find the nth root of x using Newton's method."
-  (find-root x n 1.0))
 
-(define (find-root x n guess)
-  (if (> n 0)
-    (let ((closer (improve x n guess)))
-      (if (< (abs (- guess closer)) 0.00000001)
-        closer
-        (find-root x n closer)))))
+  (define (find-root guess)
+    (if (> n 0)
+      (let ((closer (improve guess)))
+        (if (< (abs (- guess closer)) 0.00000001)
+          closer
+          (find-root closer)))))
 
-(define (improve x n guess)
-  (let ((prev-n (1- n)))
-    (/ (+ (* prev-n guess)
-          (/ x (pow guess prev-n)))
-       n)))
+  (define (improve guess)
+    (let ((prev-n (1- n)))
+      (/ (+ (* prev-n guess)
+            (/ x (pow guess prev-n)))
+         n)))
+
+  (find-root 1.0))
